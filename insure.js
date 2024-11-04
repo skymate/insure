e7d60ebfdaff07f47a01be672e8ac787ac9e4943
George Ibrahim Adeyinka <ibgeorge80@gmail.com>
2024-11-04 21:41:08 +0100

    first commit

---
+++ insure.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger")
+
+const navigation_class = document.querySelector(".navigation_class")
+
+hamburger.addEventListener("click", ()=>{
+    navigation_class.classList.hamburger("active")
+
+})
